3c59551bf00da631219a3738c7f28c4d801f4ba7
mehedi12345678910 <mdmehedi01326018868@gmail.com>
2026-04-26 22:48:26 +0600

    new writing

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const loading = () => {
   return (
     <div className="flex flex-col min-h-screen justify-center items-center gap-5">
-      <h2 className="text-5xl font-bold animate-pulse">Loading</h2>
+      <h2 className="text-5xl font-bold animate-pulse">Loading page</h2>
       <div className="animate-ping">
         <Logo></Logo>
       </div>
